5062a261771be35deee11adedad908bd8cd4959b
Cso <chanaseptiari@gmail.com>
2020-05-14 20:07:18 +0700

    Formatif 2

---
+++ application/controllers/Buku_2018310078.php
@@ -0,0 +1,20 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Buku_2018310078 extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Buku_model_2018310078','model');
+        
+    }
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Buku_2018310078.php */
